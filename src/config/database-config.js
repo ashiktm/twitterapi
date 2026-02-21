cc1c44e5e8c9cfd8c54623eb2b0ffe073b3ea3b4
ashiktm <84068768+ashiktm@users.noreply.github.com>
2026-02-21 10:14:39 +0530

    Update MongoDB connection string in database-config.js

--- src/config/database-config.js
+++ src/config/database-config.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connect = async () => {
   await mongoose.connect(
-    "mongodb+srv://ashiktmind:dKBWXZ6NaSK3ushH@cluster0.jocolvc.mongodb.net/twitterDB?retryWrites=true&w=majority"
+    "mongodb+srv://ashiktmind:dKBWXZ6NaSK3ushH@cluster0.8b7wq.mongodb.net/?appName=Cluster0"
+    
   );
 };
 
